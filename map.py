988d8ccfb739ef1c7cdde806b1785545f22fd351
Tsigai <tsigaitsgai@gmail.com>
2023-03-28 21:14:52 +0700

    Perubahan Stremalit

--- map.py
+++ map.py
@@ -1,12 +1,3 @@
-import folium
-from flask import Flask
+import streamlit as st
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    map = folium.Map()
-    return map._repr_html_()
-
-if __name__ == "__main__":
-    app.run(debug=True)
+st.button("Haiii")
